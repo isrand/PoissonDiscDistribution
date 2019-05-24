a6f5254efda93210caec9dd10a567cb203c7ee31
Isra Nebot <Israel.Nebot.Dominguez-CIC.Netherlands@ibm.com>
2019-05-24 13:16:10 +0200

    Fix Lerp function & corresponding unit tests

--- src/utils/Lerp.ts
+++ src/utils/Lerp.ts
@@ -1,5 +1,10 @@
 export function Lerp(x1: number, y1: number, x2: number, x3: number, y3: number): number {
-  const result: number = ( (x2-x1) * (y3-y1) / (x3-x1) ) + y1;
+  const topPart1: number = (x2 - x1);
+  const topPart2: number = (y3 - y1);
+  const topPartResult: number = topPart1 * topPart2;
+  const bottomPartResult: number = (x3-x1);
+  const divisionResult: number = topPartResult / bottomPartResult;
+  const result: number = divisionResult + y1;
   console.log(result);
   return result;
 }
--- test/utils/Lerp.spec.ts
+++ test/utils/Lerp.spec.ts
@@ -2,12 +2,12 @@ import { Lerp } from '../../src/utils/Lerp'
 
 test('Linear interpolation works as expected', () => {
 
-  const x1: number = 1;
-  const x2: number = 1;
-  const x3: number = 1;
-  const y1: number = 1;
-  const y3: number = 1;
+  const x1: number = 2;
+  const x2: number = 2;
+  const x3: number = 4;
+  const y1: number = 5;
+  const y3: number = 6;
 
   // Point is instantiated correctly.
-  expect(Lerp(x1, y1, x2, x3, y3)).toEqual(4);
+  expect(Lerp(x1, y1, x2, x3, y3)).toEqual(5);
 });
