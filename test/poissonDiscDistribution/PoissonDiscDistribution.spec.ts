2e244a4024e21044ea189450c141ea5f7ccd418a
Isra Nebot <israel.nebot@gmail.com>
2019-05-27 21:55:35 +0200

    Refactor PDD test

--- test/poissonDiscDistribution/PoissonDiscDistribution.spec.ts
+++ test/poissonDiscDistribution/PoissonDiscDistribution.spec.ts
@@ -4,7 +4,7 @@ import { Area2 } from '../../src/geometry/Area2';
 describe('Poisson Disc Distribution', () => {
   test('works as expected', () => {
     const area: Area2 = new Area2(400, 400, 0, 0);
-    const PDD: PoissonDiscDistribution = new PoissonDiscDistribution(area, 30, 5);
-    PDD.generateDistribution();
+
+    PoissonDiscDistribution.generateDistribution(area, 30, 5);
   });
 });
